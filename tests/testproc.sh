a8e5eb29b0a1cda7ff063f12b3e16ba75baf4ebc
cheatfate <eugene.kabanov@status.im>
2022-05-10 16:25:55 +0300

    Fix testing shell script to be compatible with macos sleep.

--- tests/testproc.sh
+++ tests/testproc.sh
@@ -4,10 +4,10 @@ if [ "$1" == "stdin" ]; then
   read -r inputdata
   echo "STDIN DATA: $inputdata"
 elif [ "$1" == "timeout2" ]; then
-  sleep 2s
+  sleep 2
   exit 2
 elif [ "$1" == "timeout10" ]; then
-  sleep 10s
+  sleep 10
 else
   echo "arguments missing"
 fi
