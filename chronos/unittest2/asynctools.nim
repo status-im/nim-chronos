b5f4c38f1c061a56113383152034edb10d482ff8
cheatfate <eugene.kabanov@status.im>
2022-06-16 03:57:03 +0300

    Initial commit.

---
+++ chronos/unittest2/asynctools.nim
@@ -0,0 +1,19 @@
+#
+#              Chronos Unittest2 Helpers
+#             (c) Copyright 2022-Present
+#         Status Research & Development GmbH
+#
+#              Licensed under either of
+#  Apache License, version 2.0, (LICENSE-APACHEv2)
+#              MIT license (LICENSE-MIT)
+import unittest2
+import ".."/asyncloop
+
+export unittest2
+
+template asyncTest*(name: string, body: untyped): untyped =
+  test name:
+    waitFor((
+      proc() {.async, gcsafe.} =
+        body
+    )())
