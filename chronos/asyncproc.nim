ec4ed00eae933e803364e1388d8554cbe06dea53
cheatfate <eugene.kabanov@status.im>
2022-09-13 18:48:42 +0300

    Fix Windows issue.

--- chronos/asyncproc.nim
+++ chronos/asyncproc.nim
@@ -576,8 +576,7 @@ when defined(windows):
         raiseAsyncProcessError("Unable to terminate process", res.error())
 
     let exitCode = p.peekProcessExitCode().valueOr:
-      raiseAsyncProcessError("Unable to peek process exit code",
-                             osErrorMsg(error))
+      raiseAsyncProcessError("Unable to peek process exit code", error)
 
     if exitCode >= 0:
       p.exitStatus = Opt.some(exitCode)
