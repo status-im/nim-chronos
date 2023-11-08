5ebd771d35464832eb9edf603b616fd34ad158cd
Jacek Sieka <jacek@status.im>
2023-11-08 15:12:32 +0100

    per-function `Exception` handling (#457)
    This PR replaces the global strict exception mode with an option to
    handle `Exception` per function while at the same time enabling strict
    exception checking globally by default as has been planned for v4.
    
    `handleException` mode raises `AsyncExceptionError` to distinguish it
    from `ValueError` which may originate from user code.
    
    * remove obsolete 1.2 config options

--- chronos/internal/errors.nim
+++ chronos/internal/errors.nim
@@ -3,3 +3,7 @@ type
     ## Generic async exception
   AsyncTimeoutError* = object of AsyncError
     ## Timeout exception
+
+  AsyncExceptionError* = object of AsyncError
+    ## Error raised in `handleException` mode - the original exception is
+    ## available from the `parent` field.
