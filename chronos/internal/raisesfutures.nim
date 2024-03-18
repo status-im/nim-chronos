b3569c3951f8b9b3111e176ac241fd8bdae73b96
Jacek Sieka <jacek@status.im>
2024-03-18 18:00:37 +0100

    workaround

--- chronos/internal/raisesfutures.nim
+++ chronos/internal/raisesfutures.nim
@@ -13,7 +13,11 @@ type
     ## and should not be used manually as the internal exception representation
     ## is subject to change in future chronos versions.
     # TODO https://github.com/nim-lang/Nim/issues/23418
-    dummy: array[0, E]
+    # TODO https://github.com/nim-lang/Nim/issues/23419
+    when E is void:
+      dummy: E
+    else:
+      dummy: array[0, E]
 
 proc makeNoRaises*(): NimNode {.compileTime.} =
   # An empty tuple would have been easier but...
