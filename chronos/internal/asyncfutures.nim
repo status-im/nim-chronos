ee93dd07e4365cae0c22e95e053a1f8515827218
cheatfate <eugene.kabanov@status.im>
2025-10-27 16:43:18 +0200

    Fix Windows terminate() race condition. Fix TRUE check on Windows where it become non-zero.

--- chronos/internal/asyncfutures.nim
+++ chronos/internal/asyncfutures.nim
@@ -1813,10 +1813,10 @@ when defined(windows):
         if res.isErr():
           retFuture.fail(newException(AsyncError, osErrorMsg(res.error())))
         else:
-          if returnFlag == TRUE:
-            retFuture.complete(WaitableResult.Timeout)
-          else:
+          if returnFlag == FALSE:
             retFuture.complete(WaitableResult.Ok)
+          else:
+            retFuture.complete(WaitableResult.Timeout)
 
     proc cancellation(udata: pointer) {.gcsafe.} =
       doAssert(not(isNil(waitHandle)))
