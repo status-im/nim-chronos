416331b9c6585c586ef25dabde8c5fcd98a4b99a
Jacek Sieka <jacek@status.im>
2026-01-13 15:20:30 +0100

    comment

--- docs/examples/cancellation.nim
+++ docs/examples/cancellation.nim
@@ -8,14 +8,15 @@ proc someTask() {.async.} =
 proc cancellationExample() {.async.} =
   # Start a task but don't wait for it to finish
   let future = someTask()
-  future.cancelSoon()
+
   # `cancelSoon` schedules but does not wait for the future to get cancelled -
   # it might still be pending here
+  future.cancelSoon()
 
   let future2 = someTask() # Start another task concurrently
   await future2.cancelAndWait()
   # Using `cancelAndWait`, we can be sure that `future2` is either
-  # complete, failed or cancelled at this point. `future` could still be
+  # complete, fail or be cancelled at this point. `future` could still be
   # pending!
   assert future2.finished()
 
