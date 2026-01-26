88581c68127c96dfb334eb1caa8cd6715e293558
Ivan Folgueira Bande <ivansete@status.im>
2026-01-26 20:27:10 +0100

    Use of reset() instead of setLen(0) to clear the underlying data

--- chronos/ioselects/ioselectors_poll.nim
+++ chronos/ioselects/ioselectors_poll.nim
@@ -56,7 +56,7 @@ proc new*(t: typedesc[Selector], T: typedesc): SelectResult[Selector[T]] =
 
 proc close2*[T](s: Selector[T]): SelectResult[void] =
   s.fds.clear()
-  s.pollfds.setLen(0)
+  s.pollfds.reset()
 
 proc new*(t: typedesc[SelectEvent]): SelectResult[SelectEvent] =
   let flags = {DescriptorFlag.NonBlock, DescriptorFlag.CloseOnExec}
