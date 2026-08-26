2d6eb1dfdd0da3b235d86104a3541aed30663dd1
Ivan FB <ivansete@status.im>
2026-08-26 22:05:16 +0200

    make sure testdispatcher is always imported

--- tests/testall.nim
+++ tests/testall.nim
@@ -13,7 +13,7 @@ import
     testmacro, testsync, testsoon, testtime, testfut, testaddress, testdatagram,
     teststream, testserver, testbugs, testnet, testasyncstream, testhttpserver,
     testshttpserver, testhttpclient, testratelimit, testfutures, testthreadsync,
-    testasyncsemaphore, testmpsc,
+    testasyncsemaphore, testmpsc, testdispatcher,
   ]
 
 when (chronosEventEngine in ["epoll", "kqueue"]) or defined(windows):
@@ -23,7 +23,7 @@ when (chronosEventEngine in ["epoll", "kqueue"]) or defined(windows):
   # Mobile test binaries cannot execute the host-side helper scripts used by
   # the process tests.
   when not (defined(android) or defined(ios)):
-    import testproc, testdispatcher
+    import testproc
 
   # Must be imported last to check for Pending futures
   import testutils
